56cf3c98d0ecc68278889245888c55ec8b6ac566
Ryan-99 <73212341+Ryan-99@users.noreply.github.com>
2024-04-08 22:12:37 +0800

    Update config.js

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -3,12 +3,12 @@ const CONFIG = {
   LANDING_HEADER_BUTTON_1_TITLE: 'Github开源',
   LANDING_HEADER_BUTTON_1_URL: 'https://github.com/tangly1024/NotionNext',
 
-  LANDING_HEADER_BUTTON_2_TITLE: '作者博客',
+  LANDING_HEADER_BUTTON_2_TITLE: '23333',
   LANDING_HEADER_BUTTON_2_URL: 'https://blog.tangly1024.com/',
 
   // 首页大图英雄板块
-  LANDING_HERO_TITLE_1: 'NotionNext',
-  LANDING_HERO_P_1: '快速搭建独立站、轻松放大品牌价值！',
+  LANDING_HERO_TITLE_1: 'EVERNOW',
+  LANDING_HERO_P_1: 'Nothing is impossible , Just do it now',
   LANDING_HERO_BUTTON_1_TEXT: '开始体验',
   LANDING_HERO_BUTTON_1_LINK: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next',
   LANDING_HERO_BUTTON_2_TEXT: '了解更多',
