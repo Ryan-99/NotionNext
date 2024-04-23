a3426c07f3182758f6467dc7138f82d9456d2b29
Ryan-99 <73212341+Ryan-99@users.noreply.github.com>
2024-04-23 22:03:14 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
-  HEO_HOME_BANNER_ENABLE: true,
+  HEO_HOME_BANNER_ENABLE: false,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-05-25', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-// # HEO_NOTICE_BAR: [
-// #   { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-//  #  { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-// # ],
+  HEO_NOTICE_BAR: [
+  { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+  { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+  ],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
@@ -24,8 +24,8 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_2: { title: '热门分享', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '实用收藏', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -113,13 +113,13 @@ const CONFIG = {
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
   HEO_MENU_INDEX: true, // 显示首页
-  HEO_MENU_CATEGORY: true, // 显示分类
+  HEO_MENU_CATEGORY: false, // 显示分类
   HEO_MENU_TAG: true, // 显示标签
   HEO_MENU_ARCHIVE: true, // 显示归档
   HEO_MENU_SEARCH: true, // 显示搜索
 
   HEO_POST_LIST_COVER: true, // 列表显示文章封面
-  HEO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
+  HEO_POST_LIST_COVER_HOVER_ENLARGE: ture, // 列表鼠标悬停放大
 
   HEO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
   HEO_POST_LIST_SUMMARY: true, // 文章摘要
